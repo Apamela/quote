ca00e53306c487a6c0e03b672e18300cd836568c
pamela sindy <pamelab.desire@gmail.com>
2019-08-25 16:52:16 +0200

    add the new codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Quote } from './quote';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+ templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,6 +11,12 @@ export class QuoteComponent implements OnInit {
     new Quote(2, 'thankfull quote', 'Gratitude can transform common days into thanksgivings, turn routine jobs into joy, and change ordinary opportunities into blessings.',new Date(2019,3,4)),
     new Quote(3, 'succeess quote' , 'Success is not final; failure is not fatal: It is the courage to continue that counts',new Date(2019,6,8)),
   ];
+  addNewGoal(goal){
+    let goalLength = this.quotes.length;
+    goal.id = goalLength+1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.quotes.push(goal)
+  }
   deleteQuote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
